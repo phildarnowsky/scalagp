dde0ea5e50f3e73fc5ab5e3aa82088a0ec2b2602
Phil Darnowsky <phil@darnowsky.com>
2012-10-08 22:11:35 -0400

    created a few example boolean node functions

---
+++ scalagp-examples/src/main/scala/nodefunction/boolean/Constants.scala
@@ -0,0 +1,12 @@
+package com.darnowsky.scalagp_examples.nodefunction.boolean.Constants
+
+import com.darnowsky.scalagp.NodeFunction._
+import com.darnowsky.scalagp.ProgramNode.ProgramNode
+
+object FalseNodeFunction extends TerminalNodeFunction[Boolean] {
+  def apply(_children: Seq[ProgramNode[Boolean]]) = false
+}
+
+object TrueNodeFunction extends TerminalNodeFunction[Boolean] {
+  def apply(_children: Seq[ProgramNode[Boolean]]) = true
+}
--- scalagp-examples/src/main/scala/nodefunction/boolean/TrueNodeFunction.scala
+++
@@ -1,8 +0,0 @@
-package com.darnowsky.scalagp_examples.nodefunction.boolean.TrueNodeFunction
-
-import com.darnowsky.scalagp.NodeFunction._
-import com.darnowsky.scalagp.ProgramNode.ProgramNode
-
-class TrueNodeFunction extends TerminalNodeFunction[Boolean] {
-  def apply(_children: Seq[ProgramNode[Boolean]]) = true
-}

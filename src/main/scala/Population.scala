687d01454741bd736a83bdde76391a32a0ede3d9
Phil Darnowsky <phil@darnowsky.com>
2012-10-26 23:54:59 -0400

    generateRampedHalfAndHalf convenience method

--- src/main/scala/Population.scala
+++ src/main/scala/Population.scala
@@ -1,7 +1,10 @@
 package com.darnowsky.scalagp.Population
 
 import com.darnowsky.scalagp.ProgramGenerationStrategy.ProgramGenerationStrategy
+import com.darnowsky.scalagp.FullGenerationStrategy.FullGenerationStrategy
+import com.darnowsky.scalagp.GrowGenerationStrategy.GrowGenerationStrategy
 import com.darnowsky.scalagp.ProgramNode.ProgramNode
+import com.darnowsky.scalagp.NodeFunction.{NonterminalNodeFunctionCreator, TerminalNodeFunctionCreator}
 
 object Population {
   def generate[T](
@@ -14,6 +17,22 @@ object Population {
         (1 to trancheSize).map(_ => strategy.generateProgram(depth))))
 
     new Population[T](programs)
+  }
+
+  // Convenience method to generate population by the popular ramped half-and-
+  // half method.
+  def generateRampedHalfAndHalf[T](
+    trancheSize: Int,
+    maximumDepth: Int,
+    nonterminals: Seq[NonterminalNodeFunctionCreator[T]],
+    terminals: Seq[TerminalNodeFunctionCreator[T]]
+  ) ={
+    val strategies = List(
+      new FullGenerationStrategy(nonterminals, terminals),
+      new GrowGenerationStrategy(nonterminals, terminals)
+    )
+
+    generate(trancheSize, maximumDepth, strategies)
   }
 }
 

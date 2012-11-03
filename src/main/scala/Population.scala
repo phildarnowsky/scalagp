8870bfd138e38f8a935aa8fffc5e922121ee5547
Phil Darnowsky <phil@darnowsky.com>
2012-11-02 21:59:30 -0400

    added evaluation functions to Population

--- src/main/scala/Population.scala
+++ src/main/scala/Population.scala
@@ -7,34 +7,47 @@ import com.darnowsky.scalagp.ProgramNode.ProgramNode
 import com.darnowsky.scalagp.NodeFunction.{NonterminalNodeFunctionCreator, TerminalNodeFunctionCreator}
 
 object Population {
-  def generate[T](
+  def generate[ProgramType, FitnessType](
     trancheSize: Int, 
     maximumDepth: Int, 
-    generationStrategies: Seq[ProgramGenerationStrategy[T]]): Population[T] = {
+    generationStrategies: Seq[ProgramGenerationStrategy[ProgramType]],
+    evaluationFunction: ProgramFitnessFunction[ProgramType, FitnessType]
+  ): Population[ProgramType, FitnessType] = {
 
     val programs = (1 to maximumDepth).flatMap(depth => 
       generationStrategies.flatMap(strategy =>
         (1 to trancheSize).map(_ => strategy.generateProgram(depth))))
 
-    new Population[T](programs)
+    new Population(programs, evaluationFunction)
   }
 
   // Convenience method to generate population by the popular ramped half-and-
   // half method.
-  def generateRampedHalfAndHalf[T](
+  def generateRampedHalfAndHalf[ProgramType, FitnessType] (
     trancheSize: Int,
     maximumDepth: Int,
-    nonterminals: Seq[NonterminalNodeFunctionCreator[T]],
-    terminals: Seq[TerminalNodeFunctionCreator[T]]
-  ) ={
+    nonterminals: Seq[NonterminalNodeFunctionCreator[ProgramType]],
+    terminals: Seq[TerminalNodeFunctionCreator[ProgramType]],
+    evaluationFunction: ProgramFitnessFunction[ProgramType, FitnessType]
+  ) = {
     val strategies = List(
       new FullGenerationStrategy(nonterminals, terminals),
       new GrowGenerationStrategy(nonterminals, terminals)
     )
 
-    generate(trancheSize, maximumDepth, strategies)
+    generate(trancheSize, maximumDepth, strategies, evaluationFunction)
   }
 }
 
-class Population[T](val programs: Seq[ProgramNode[T]]) {
+case class Population[ProgramType, FitnessType](
+  val programs: Seq[ProgramNode[ProgramType]], 
+  val fitnessFunction: ProgramFitnessFunction[ProgramType, FitnessType]
+) {
+  def fitnesses(): Map[ProgramNode[ProgramType], FitnessType] = {
+    val emptyMap = new scala.collection.immutable.HashMap[ProgramNode[ProgramType], FitnessType]
+    programs.foldLeft(emptyMap)((map, program) => map + (program -> fitnessFunction(program.evaluate())))
+  }
+}
+
+abstract class ProgramFitnessFunction[InputType, OutputType] extends Function1[InputType, OutputType] {
 }
--- src/test/scala/PopulationSpec.scala
+++ src/test/scala/PopulationSpec.scala
@@ -9,35 +9,57 @@ import com.darnowsky.scalagp.ProgramNode.ProgramNode
 import com.darnowsky.scalagp.Population._
 
 class PopulationSpec extends Specification with Mockito {
+  object TestProgramFitnessFunction extends ProgramFitnessFunction[Int, Double] {
+    def apply(programOutput: Int): Double = programOutput * 3.0
+  }
+
+  object Nonterminal1 extends NonterminalNodeFunction[Int](2) {
+    def apply(nodes: Seq[ProgramNode[Int]]) = nodes.head.evaluate
+    def toIdentifier = "first"
+  }
+
+  object Nonterminal2 extends NonterminalNodeFunction[Int](2) {
+    def apply(nodes: Seq[ProgramNode[Int]]) = nodes.last.evaluate
+    def toIdentifier = "last"
+  }
+
+  object Terminal1 extends TerminalNodeFunction[Int] {
+    def apply(nodes: Seq[ProgramNode[Int]]) = 123
+    def toIdentifier = "123"
+  }
+
+  object Terminal2 extends TerminalNodeFunction[Int] {
+    def apply(nodes: Seq[ProgramNode[Int]]) = 456
+    def toIdentifier = "456"
+  }
+
+
   "The Population companion object" should {
     "be able to generate a Population via ramped even splits" in {
-      object Nonterminal1 extends NonterminalNodeFunction[Int](2) {
-        def apply(nodes: Seq[ProgramNode[Int]]) = nodes.head.evaluate
-        def toIdentifier = "first"
-      }
-
-      object Nonterminal2 extends NonterminalNodeFunction[Int](2) {
-        def apply(nodes: Seq[ProgramNode[Int]]) = nodes.last.evaluate
-        def toIdentifier = "last"
-      }
-
-      object Terminal1 extends TerminalNodeFunction[Int] {
-        def apply(nodes: Seq[ProgramNode[Int]]) = 123
-        def toIdentifier = "123"
-      }
-
-      object Terminal2 extends TerminalNodeFunction[Int] {
-        def apply(nodes: Seq[ProgramNode[Int]]) = 456
-        def toIdentifier = "456"
-      }
-
       val fullStrategy = spy(new FullGenerationStrategy[Int](List(Nonterminal1, Nonterminal2), List(Terminal1, Terminal2)))
       val growStrategy = spy(new GrowGenerationStrategy[Int](List(Nonterminal1, Nonterminal2), List(Terminal1, Terminal2)))
 
-      val population = Population.generate(3, 5, List(fullStrategy, growStrategy))
+      val population = Population.generate(3, 5, List(fullStrategy, growStrategy), TestProgramFitnessFunction)
 
       (1 to 5).foreach(expectedDepth => there was 3.times(fullStrategy).generateProgram(expectedDepth))
       (1 to 5).foreach(expectedDepth => there was 3.times(growStrategy).generateProgram(expectedDepth))
+    }
+  }
+
+  "A Population" should {
+    "evaluate its constituent programs for fitness" in {
+      object TerminalProgramGenerationStrategy extends ProgramGenerationStrategy[Int](List(), List()) {
+        def generateChildren(depth: Int, arity: Int): Seq[ProgramNode[Int]] = List(Terminal1, Terminal2).map(new ProgramNode[Int](_, this, depth - 1))
+      }
+
+      val testProgram1 = new ProgramNode(Nonterminal1, TerminalProgramGenerationStrategy, 2)
+      val testProgram2 = new ProgramNode(Nonterminal2, TerminalProgramGenerationStrategy, 2)
+      val population = new Population(List(testProgram1, testProgram2), TestProgramFitnessFunction)
+
+      val fitnessResult = population.fitnesses
+      fitnessResult.size mustEqual 2
+      fitnessResult(testProgram1) mustEqual 369.0
+      fitnessResult(testProgram2) mustEqual 1368.0
     }
   }
 }

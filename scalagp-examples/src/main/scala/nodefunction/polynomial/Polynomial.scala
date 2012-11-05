5952251090fbb12894efca80b64ab50366ac052f
Phil Darnowsky <phil@darnowsky.com>
2012-11-04 22:14:38 -0500

    made population for symbolic regression with polynomials

---
+++ scalagp-examples/src/main/scala/nodefunction/polynomial/Polynomial.scala
@@ -0,0 +1,83 @@
+package com.darnowsky.scalagp_examples.nodefunction.Polynomial
+
+import com.darnowsky.scalagp.NodeFunction._
+import com.darnowsky.scalagp.ProgramNode.ProgramNode
+
+class Monomial(val coefficient:Double, val power:Int) {
+  override def toString = {
+    if(coefficient == 0.0)
+      "0"
+    else
+      power match {
+        case 0 => coefficient.toString
+        case 1 => coefficient.toString ++ "X"
+        case _ => coefficient.toString ++ "X^" ++ power.toString
+      }
+  }
+
+  def * (that: Monomial): Monomial = {
+    new Monomial(this.coefficient * that.coefficient, this.power + that.power)
+  }
+
+  def + (that: Monomial): Polynomial = new Polynomial(List(this, that))
+
+  def apply(x: Double): Double = coefficient * scala.math.pow(x, power)
+}
+
+object Monomial {
+  def X = new Monomial(1, 1)
+}
+
+class Polynomial(val terms:Seq[Monomial]) {
+  override def toString = terms.map(_.toString) mkString " + "
+
+  def * (that: Polynomial): Polynomial = {
+    val newTerms = this.terms.flatMap((leftTerm) => that.terms.map((rightTerm) => leftTerm * rightTerm))
+    new Polynomial(newTerms)
+  }
+
+  def + (that: Polynomial): Polynomial = {
+    new Polynomial(this.terms ++ that.terms)
+  }
+
+  def apply(x: Double): Double = terms.map(_(x)).sum
+}
+
+abstract class BinaryOperator extends NonterminalNodeFunction[Polynomial](2) {
+}
+
+object Add extends BinaryOperator {
+  def apply(children: Seq[ProgramNode[Polynomial]]): Polynomial = {
+    children(0).evaluate() + children(1).evaluate()
+  }
+
+  def toIdentifier = "+"
+}
+
+object Multiply extends BinaryOperator {
+  def apply(children: Seq[ProgramNode[Polynomial]]): Polynomial = {
+    children(0).evaluate() * children(1).evaluate()
+  }
+
+  def toIdentifier = "*"
+}
+
+object IndependentVariable extends TerminalNodeFunction[Polynomial] {
+  def apply(children: Seq[ProgramNode[Polynomial]]) = new Polynomial(List(Monomial.X))
+
+  def toIdentifier = "X"
+}
+
+class ERCNodeFunction(lower: Double, upper: Double) extends TerminalNodeFunction[Polynomial] {
+  require(upper > lower)
+  val spread = upper - lower
+  lazy val constantValue = (new scala.util.Random).nextDouble * spread + lower
+
+  def apply(_children: Seq[ProgramNode[Polynomial]]) = new Polynomial(List(new Monomial(constantValue, 0)))
+
+  def toIdentifier = constantValue.toString
+}
+
+class ERCNodeFunctionCreator(lower: Double, upper: Double) extends TerminalNodeFunctionCreator[Polynomial] {
+  def getNodeFunction(): ERCNodeFunction = new ERCNodeFunction(lower, upper)
+}
---
+++ scalagp-examples/src/main/scala/population/SymbolicRegression.scala
@@ -0,0 +1,41 @@
+package com.darnowsky.scalagp_examples.population.SymbolicRegression
+
+import com.darnowsky.scalagp_examples.nodefunction.Polynomial._
+import com.darnowsky.scalagp.Population._
+
+class SymbolicRegressionFitnessFunction(val datapoints:Seq[(Double, Double)]) extends ProgramFitnessFunction[Polynomial, Double] {
+  def apply(polynomial: Polynomial) = {
+    val errors = datapoints.map((datapoint) => {
+      val x = datapoint._1
+      val y = datapoint._2
+      val calculatedY = polynomial(x)
+      val difference = y - calculatedY
+      difference * difference
+    })
+
+    errors.sum
+  }
+}
+
+// These datapoints were chosen randomly
+
+object RandomRegressionFitnessFunction extends SymbolicRegressionFitnessFunction(List((0.0, 35.48), (1.0, 34.48), (2.0, 3.11), (3.0, 55.35), (4.0, 88.76), (5.0, 55.67), (6.0, 19.85), (7.0, 60.22), (8.0, 15.92), (9.0, 77.71))) {}
+
+// And these are from the gnuplot tutorial "free fall" example:
+
+object FreefallRegressionFitnessFunction extends SymbolicRegressionFitnessFunction(List((0.0, 0.0), (0.5, 1.3), (0.75, 2.8), (1.0, 4.8), (1.25, 7.8)))
+
+object PolynomialPopulation {
+  def create(
+    trancheSize: Int, 
+    maximumDepth: Int, 
+    fitnessFunction: SymbolicRegressionFitnessFunction): Population[Polynomial, Double] = {
+    Population.generateRampedHalfAndHalf(
+      trancheSize,
+      maximumDepth,
+      List(Add, Multiply),
+      List(IndependentVariable, new ERCNodeFunctionCreator(-10.0, 10.0)),
+      fitnessFunction
+    )
+  }
+}

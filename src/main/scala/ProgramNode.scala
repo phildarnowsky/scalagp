294bf5487de52212304decfe661299220f7635f4
Phil Darnowsky <phil@darnowsky.com>
2012-12-30 01:28:59 -0500

    crossover works

--- src/main/scala/ProgramNode.scala
+++ src/main/scala/ProgramNode.scala
@@ -22,7 +22,14 @@ case class ProgramNode[T](
     evaluationFunction(children)
   }
 
-  def crossoverWith(that: ProgramNode[T]): (ProgramNode[T], ProgramNode[T]) = (this, that)
+  def crossoverWith(that: ProgramNode[T]): (ProgramNode[T], ProgramNode[T]) = {
+    val thisCrossoverPoint = this.chooseRandomDescendant()
+    val thatCrossoverPoint = that.chooseRandomDescendant()
+
+    val thisNewTree = this.insertReplacementSubtree(thatCrossoverPoint, thisCrossoverPoint.pathFromRoot)
+    val thatNewTree = that.insertReplacementSubtree(thisCrossoverPoint, thatCrossoverPoint.pathFromRoot)
+    (thisNewTree, thatNewTree)
+  }
 
   def toSExpression(): String = {
     if(children.isEmpty)
@@ -32,4 +39,34 @@ case class ProgramNode[T](
   }
 
   lazy val allDescendants: IndexedSeq[ProgramNode[T]] = children.flatMap(_.allDescendants) :+ this
+
+  def chooseRandomDescendant(): ProgramNode[T] = allDescendants(rng.nextInt(allDescendants.length))
+
+  protected
+
+  lazy val rng = new scala.util.Random
+
+  def insertReplacementSubtree(that: ProgramNode[T], position: Queue[Int]): ProgramNode[T] = {
+    val newSubtree = that.updatePathFromRoot(position)
+    // todo: map over children applying copyToPosition to them to fix up positions
+    if(position.isEmpty) {
+      newSubtree
+    } else {
+      val subtreeIndex = position.last
+      val pathToParent = position.init
+      val parentToReplace = this.followPath(pathToParent)
+
+      val leftChildren = parentToReplace.children.take(subtreeIndex)
+      val rightChildren = parentToReplace.children.drop(subtreeIndex + 1)
+      val replacedParent = parentToReplace.copy(children = leftChildren ++ Array(newSubtree) ++ rightChildren)
+      insertReplacementSubtree(replacedParent, pathToParent)
+    }
+  }
+
+  def updatePathFromRoot(newPathFromRoot: Queue[Int]): ProgramNode[T] = {
+    val newChildren = children.toIndexedSeq.zipWithIndex.map((nodeTuple) => nodeTuple._1.updatePathFromRoot(newPathFromRoot :+ nodeTuple._2))
+    copy(pathFromRoot = newPathFromRoot, children = newChildren) 
+  }
+
+  def followPath(path: Queue[Int]): ProgramNode[T] = path.foldLeft(this)((program, index) => program.children(index))
 }
--- src/test/scala/ProgramNodeSpec.scala
+++ src/test/scala/ProgramNodeSpec.scala
@@ -2,87 +2,141 @@ import org.specs2.mutable._
 
 import com.darnowsky.scalagp.ProgramNode._
 import com.darnowsky.scalagp.ProgramGenerationStrategy._
-import com.darnowsky.scalagp.NodeFunction.{TerminalNodeFunction, NonterminalNodeFunction, NodeFunctionCreator}
+import com.darnowsky.scalagp.NodeFunction._
 import org.specs2.mock._
 import scala.collection.immutable.Queue
 
 class ProgramNodeSpec extends Specification with Mockito with SpecHelpers {
-  "A ProgramNode" should {
-    "be able to do crossover reproduction with another ProgramNode" in {  
-      // program1 is (+ 42 (- 666))
-      // program2 is (- (* (succ 1337) (+ 3456 78910)))
-      // TODO: make an easier way to set up these fixed programs
+  def allNodeFunctions[T](programs: List[ProgramNode[T]]): List[NodeFunction[T]] = programs.flatMap(_.allDescendants).map(_.evaluationFunction)
 
-      val program1 =  new ProgramNode[Int](
-        AddEvaluationFunction,
-        Array(
-          new ProgramNode[Int](
-            ConstantEvaluationFunction42,
-            Array():Array[ProgramNode[Int]],
-            Queue(0)
+  "A ProgramNode" should {      
+    // TODO: make an easier way to set up these fixed programs
+
+    val program1 =  new ProgramNode[Int](
+      AddEvaluationFunction,
+      Array(
+        new ProgramNode[Int](
+          ConstantEvaluationFunction42,
+          Array():Array[ProgramNode[Int]],
+          Queue(0)
+        ),
+
+        new ProgramNode[Int](
+          NegateEvaluationFunction,
+          Array(
+            new ProgramNode[Int](
+              ConstantEvaluationFunction666,
+              Array():Array[ProgramNode[Int]],
+              Queue(1, 0)
+            )
           ),
-
-          new ProgramNode[Int](
-            NegateEvaluationFunction,
-            Array(
-              new ProgramNode[Int](
-                ConstantEvaluationFunction666,
-                Array():Array[ProgramNode[Int]],
-                Queue(1, 0)
-              )
-            ),
-            Queue(1)
-          )
+          Queue(1)
         )
       )
+    )
 
-      val program2 = new ProgramNode[Int](
-        NegateEvaluationFunction,
-        Array(
+    val program2 = new ProgramNode[Int](
+      NegateEvaluationFunction,
+      Array(
 
-          new ProgramNode[Int](
-            MultiplyEvaluationFunction,
-            Array(
+        new ProgramNode[Int](
+          MultiplyEvaluationFunction,
+          Array(
 
-              new ProgramNode[Int](
-                SuccessorEvaluationFunction,
-                Array(
+            new ProgramNode[Int](
+              SuccessorEvaluationFunction,
+              Array(
 
-                  new ProgramNode[Int](
-                    ConstantEvaluationFunction1337,
-                    Array():Array[ProgramNode[Int]],
-                    Queue(0, 0, 0)
-                  )
+                new ProgramNode[Int](
+                  ConstantEvaluationFunction1337,
+                  Array():Array[ProgramNode[Int]],
+                  Queue(0, 0, 0)
+                )
 
-                ),
-                Queue(0, 0)
               ),
-
-              new ProgramNode[Int](
-                AddEvaluationFunction,
-                Array(
-                  new ProgramNode[Int](
-                    ConstantEvaluationFunction3456,
-                    Array():Array[ProgramNode[Int]],
-                    Queue(0, 1, 0)
-                  ),
-
-                  new ProgramNode[Int](
-                    ConstantEvaluationFunction78910,
-                    Array():Array[ProgramNode[Int]],
-                    Queue(0, 1, 1)
-                  )
-                ),
-                Queue(0, 1)
-              )
+              Queue(0, 0)
             ),
-            Queue(0)
-          )
+
+            new ProgramNode[Int](
+              AddEvaluationFunction,
+              Array(
+                new ProgramNode[Int](
+                  ConstantEvaluationFunction3456,
+                  Array():Array[ProgramNode[Int]],
+                  Queue(0, 1, 0)
+                ),
+
+                new ProgramNode[Int](
+                  ConstantEvaluationFunction78910,
+                  Array():Array[ProgramNode[Int]],
+                  Queue(0, 1, 1)
+                )
+              ),
+              Queue(0, 1)
+            )
+          ),
+          Queue(0)
         )
       )
+    )
 
-      val(child1, child2) = program1 crossoverWith program2
-      pending
+    // program1 is (+ 42 (- 666))
+    // program2 is (- (* (succ 1337) (+ 3456 78910)))
+
+    "handle the case in which it crosses over at the root" in {
+      val spiedProgram1 = spy(program1)
+      val spiedProgram2 = spy(program2)
+
+      spiedProgram1.chooseRandomDescendant().returns(spiedProgram1) // choose the root
+      spiedProgram2.chooseRandomDescendant().returns(spiedProgram2.children(0).children(1)) // choose (+ 3456 78910)
+      val(child1, child2) = spiedProgram1 crossoverWith spiedProgram2
+
+      child1.evaluate() mustEqual (3456 + 78910)
+      child2.evaluate() mustEqual -(1338 * (42 - 666))
+
+      child1.toSExpression mustEqual "(+ 3456 78910)"
+      child2.toSExpression mustEqual "(- (* (succ 1337) (+ 42 (- 666))))"
+
+      child1.pathFromRoot.isEmpty must beTrue
+      child1.children(0).pathFromRoot mustEqual Queue(0)
+      child1.children(1).pathFromRoot mustEqual Queue(1)
+
+      child2.pathFromRoot.isEmpty must beTrue // -
+      child2.children(0).pathFromRoot mustEqual Queue(0) // *
+      child2.children(0).children(0).pathFromRoot mustEqual Queue(0, 0) // succ
+      child2.children(0).children(0).children(0).pathFromRoot mustEqual Queue(0, 0, 0) // 1337
+      child2.children(0).children(1).pathFromRoot mustEqual Queue(0, 1) // +
+      child2.children(0).children(1).children(0).pathFromRoot mustEqual Queue(0, 1, 0) // 42
+      child2.children(0).children(1).children(1).pathFromRoot mustEqual Queue(0, 1, 1) // -
+      child2.children(0).children(1).children(1).children(0).pathFromRoot mustEqual Queue (0, 1, 1, 0) // 666
+    }
+
+    "be able to do crossover reproduction with another ProgramNode" in {  
+      val spiedProgram1 = spy(program1)
+      val spiedProgram2 = spy(program2)
+
+      spiedProgram1.chooseRandomDescendant().returns(spiedProgram1.children(0)) // choose the 42 node
+      spiedProgram2.chooseRandomDescendant().returns(spiedProgram2.children(0).children(1)) // choose (+ 3456 78910)
+      val(child1, child2) = spiedProgram1 crossoverWith spiedProgram2
+
+      child1.evaluate() mustEqual (3456 + 78910 - 666)
+      child2.evaluate() mustEqual -(1338 * 42)
+
+      child1.toSExpression mustEqual "(+ (+ 3456 78910) (- 666))"
+      child2.toSExpression mustEqual "(- (* (succ 1337) 42))"
+
+      child1.pathFromRoot.isEmpty must beTrue // outer +
+      child1.children(0).pathFromRoot mustEqual Queue(0) // inner +
+      child1.children(0).children(0).pathFromRoot mustEqual Queue(0, 0) // 3456
+      child1.children(0).children(1).pathFromRoot mustEqual Queue(0, 1) // 78910
+      child1.children(1).pathFromRoot mustEqual Queue(1) // -
+      child1.children(1).children(0).pathFromRoot mustEqual Queue(1, 0) // 666
+
+      child2.pathFromRoot.isEmpty must beTrue // -
+      child2.children(0).pathFromRoot mustEqual Queue(0) // *
+      child2.children(0).children(0).pathFromRoot mustEqual Queue(0, 0) // succ
+      child2.children(0).children(0).children(0).pathFromRoot mustEqual Queue(0, 0, 0) // 1337
+      child2.children(0).children(1).pathFromRoot mustEqual Queue(0, 1) // 42
     }  
 
     "be able to evaluate itself" in {

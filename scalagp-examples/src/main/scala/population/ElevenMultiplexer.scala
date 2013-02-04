1b1cc405b66a123bcd65517d27670ea52bc5030c
Phil Darnowsky <phil@darnowsky.com>
2013-02-03 20:13:11 -0500

    wrote fitness function for 11-multiplexer

--- scalagp-examples/src/main/scala/population/ElevenMultiplexer.scala
+++ scalagp-examples/src/main/scala/population/ElevenMultiplexer.scala
@@ -1,8 +1,21 @@
-package com.darnowsky.scalagp_examples.population.SymbolicRegression
+package com.darnowsky.scalagp_examples.population.ElevenMultiplexer
 
 import com.darnowsky.scalagp.Population._
 import com.darnowsky.scalagp_examples.nodefunction.booleanfunction.ElevenMultiplexer._
 
-//object ElevenMultiplexerFitnessFunction extends ProgramFitnessFunction[BooleanFunction] {
-  //def apply(booleanFunction: BooleanFunction) = 44.0
-//}
+class ElevenMultiplexerFitnessCase(caseIndex: Int) {
+  val bits = Vector(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024).map{(mask: Int) => (caseIndex & mask) > 0}
+
+  val correctResult = {
+    val address = caseIndex & 7
+    bits(address + 3)
+  }
+
+  def incorrect(booleanFunction: BooleanFunction) = booleanFunction(bits) != correctResult
+}
+
+object ElevenMultiplexerFitnessFunction extends ProgramFitnessFunction[BooleanFunction] {
+  val fitnessCases = (0 to 2047).map(new ElevenMultiplexerFitnessCase(_))
+
+  def apply(booleanFunction: BooleanFunction) = fitnessCases.count((fitnessCase) => fitnessCase.incorrect(booleanFunction)).toDouble
+}
